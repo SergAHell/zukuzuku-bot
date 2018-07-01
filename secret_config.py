aa519ef60a5482aef40b1a8f9e252a211d40e090
Forden <mr.i.lebedev2@gmail.com>
2018-07-01 17:01:21 +0300

    Удаление лишних файлов из репозитория Фикс бага с неудачной регистрацией чата при ошибке

--- secret_config.py
+++
@@ -1,9 +0,0 @@
-#coding: utf8
-
-token = '495038140:AAEPM7yysLhXSqcHBIFLo9LEvpnsOd5MCjo'
-test_token = '457048468:AAG29K2kyK1faFbO4D01lq6Jif2SW5O58P0'
-
-host = '31.202.128.8'
-user = 'zukuzuku'
-password = 'nXxCua1C4PeevRtQ'
-db = 'zukuzuku'
